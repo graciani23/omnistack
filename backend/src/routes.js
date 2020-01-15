eb120ae5a6a9b99dcb8a120e890c478468ac1b46
graciani23 <gracianigss@gmail.com>
2020-01-15 17:39:50 -0300

    consumindo api do github

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,27 @@
 const { Router } = require('express'); // importando apenas o Router
-
+const axios = require('axios'); // para consumir api do github
+const Dev = require('./models/Dev');
 const routes = Router();
 
-routes.post('/devs', (req, res) => {
-    console.log(req.body);
-    return res.json({ message: 'Hello Omnistack!' });
+routes.post('/devs', async (req, res) => {
+    const { github_username, techs } = req.body;
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = apiResponse.data;
+    // name = login - se o name não estiver cadastrado trará o login
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    });
+    
+    return res.json(dev);
 });
 
 module.exports = routes;
